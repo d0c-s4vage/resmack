b4fa9c78408dd3235270477de960760911172599
James Johnson <d0c.s4vage@gmail.com>
2020-10-21 21:41:19 -0700

    Now have tests for all items

---
+++ test/test_str.cpp
@@ -0,0 +1,45 @@
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "types.hpp"
+#include "rules.hpp"
+#include "item.hpp"
+#include "items/str.hpp"
+
+#include "test_utils.hpp"
+
+namespace resmack {
+namespace items {
+
+  TEST(Str, CorrectLength) {
+    for (size_t i = 0; i < 100; i++) {
+      Str* str = new Str(2, 3, "A");
+      std::string built = test_utils::BuildItem(str);
+      EXPECT_EQ(built, "AA");
+    }
+
+    Map<std::string, size_t> counts;
+    // 2, 3, 4 are the possible lengths
+    Str* str = new Str(2, 5, "A");
+    test_utils::CountBuilds(100, str, &counts);
+
+    EXPECT_GT(counts["AA"], 0u);
+    EXPECT_GT(counts["AAA"], 0u);
+    EXPECT_GT(counts["AAA"], 0u);
+  }
+
+  TEST(Str, FullCharset) {
+    Map<std::string, size_t> counts;
+    // 2, 3, 4 are the possible lengths
+    Str* str = new Str(1, 2, "ABCD");
+    test_utils::CountBuilds(1000, str, &counts);
+
+    EXPECT_GT(counts["A"], 0u);
+    EXPECT_GT(counts["B"], 0u);
+    EXPECT_GT(counts["C"], 0u);
+    EXPECT_GT(counts["D"], 0u);
+  }
+
+}
+}
