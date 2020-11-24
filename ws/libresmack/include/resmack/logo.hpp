fdaaedb9584e10488c933ecb557c8a241f388836
James Johnson <d0c.s4vage@gmail.com>
2020-11-24 09:55:26 -0800

    logo

---
+++ ws/libresmack/include/resmack/logo.hpp
@@ -0,0 +1,10 @@
+#ifndef RESMACK_BANNER_H
+#define RESMACK_BANNER_H
+
+namespace resmack {
+
+  const char* GetResmackLogo();
+
+}
+
+#endif
---
+++ ws/libresmack/src/logo.cpp
@@ -0,0 +1,21 @@
+#include "resmack/logo.hpp"
+
+
+namespace resmack {
+
+  const char* GetResmackLogo() {
+    static const char* banner = 
+      "\n"
+      "                                 ░    \n"
+      "             ╭───┐╭─┬─┐╭───┐╭───┐▒  ╱ \n"
+      "   █━━━┓┏━━━┓│    │ │ │    ││   │▓ ╱  \n"
+      "   █    ┣━━━┛└───┐│ │ │┌───┤│    █◀   \n"
+      "   ▓    ┃        ││   ││   ││    █ ╲  \n"
+      "   ▒    ┗━━━┛└───╯│   │└───┤└───╯█  ╲ \n"
+      "   ░              │   │              ╲\n"
+      "\n"
+      "      by James 'd0c-s4vage' Johnson   \n";
+    return banner;
+  }
+
+}
